3109d637de43f3fcfa828ab4b0728995c47959fd
Lucas Fontenele <lucaspfonte@usp.br>
2025-02-14 13:07:39 +0000

    fatorial

---
+++ submission/fatorial.asm
@@ -0,0 +1,33 @@
+@ /100
+N   K /0000 ; Input do numero
+RES K /0000 ; Resultado do fatorial
+UM  K /0001 ; valor 1 a ser decrementado
+
+@ /200
+SC  FATORIAL
+JP  HALT
+
+@ /300
+FATORIAL K /0000
+    LD  N ; AC = N 
+    JN  HALT ; Encerra se N<0
+    JZ  ZERO
+    LOOP MM RES; Salva AC no resultado
+         LD N; Carrega valor intermediario de n
+         SB UM; AC = AC-1
+         JZ END_FATORIAL
+         MM N ; N = N-1
+         ML RES; AC = (n-1) x (resultado atual)
+         JP LOOP
+END_FATORIAL RS FATORIAL
+
+
+
+@ /400
+ZERO LV = 1 ; AC = 1
+     MM RES ; RES = 1
+HALT HM = 0
+
+
+
+
